36796da4211c264adcc6a0c0aced068a8e4ac887
rubensojunior <rubensojunior6@gmail.com>
2020-04-15 14:16:26 -0300

    working on transaction post route

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,26 +1,40 @@
-import { Router } from 'express';
+import { Router } from 'express'
+import { uuid } from 'uuidv4'
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
 // import CreateTransactionService from '../services/CreateTransactionService';
 
-const transactionRouter = Router();
+const transactionRouter = Router()
 
 // const transactionsRepository = new TransactionsRepository();
+
+const transactions = []
 
 transactionRouter.get('/', (request, response) => {
   try {
     // TODO
   } catch (err) {
-    return response.status(400).json({ error: err.message });
+    return response.status(400).json({ error: err.message })
   }
-});
+})
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
-});
+    const { title, value, type } = request.body
 
-export default transactionRouter;
+    const transaction = {
+      id: uuid(),
+      title,
+      value,
+      type,
+    }
+
+    transactions.push(transaction)
+
+    return response.json(transaction)
+  } catch (err) {
+    return response.status(400).json({ error: err.message })
+  }
+})
+
+export default transactionRouter
